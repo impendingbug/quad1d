dd10ddc21b2990f2538038c8265bdb3bc1efb8f1
impendingbug <impendingbug@users.noreply.github.com>
2016-01-09 21:20:28 +0100

    Update test_cag_inf.cpp

--- test/test_cag_inf.cpp
+++ test/test_cag_inf.cpp
@@ -17,63 +17,37 @@
  *  This source code is released for free distribution under the terms of the
  *  GNU General Public License as published by the Free Software Foundation.
  */
-
-#include <boost/math/constants/constants.hpp>
-
 #include "quad1d.hpp"
-
-#include <cmath>
-#include <cstddef>
-#include <iostream>
+#include <complex>
 #include <stdexcept>
-
-#define SPIT(n) (std::cout << #n " = " << n << std::endl)
+#include <iostream>
 
 using namespace std;
-using cx_double = complex<double>;
 
-cx_double f_expected(cx_double p_sqr, double q) {
-    if (p_sqr.real() <= 0.) throw logic_error("f_expected() : re(p_sqr) must be > 0)");
-    return exp(0.25 * q * q / p_sqr) * sqrt(M_PI / p_sqr);
+complex<double> f_expected(const complex<double>& p, double q) {  //the closed-form solution
+    if (p.real() <= 0.) throw logic_error("f_expected() : re(p) must be > 0)");
+    return exp(0.25 * q * q / p) * sqrt(M_PI / p);
 }
-
-cx_double f_integrand(double x, cx_double p_sqr, double q) {
-    return exp(-p_sqr * x * x + q * x);
+complex<double> f_integrand(double x, const complex<double>& p, double q) {  //the integrand
+    return exp(-p * x * x + q * x);
 }
-
-struct ftor {
-    ftor(cx_double p_sqr, double q): p_sqr(p_sqr), q(q) { }
-    cx_double operator()(double x) const { ++counter; return f_integrand(x, p_sqr, q); }
-    cx_double p_sqr;
+struct Ftor {  //a wrapper functor
+    Ftor(complex<double> p, double q): p(p), q(q) { }
+    complex<double> operator()(double x) const { return f_integrand(x, p, q); }
+    complex<double> p;
     double q;
-    mutable size_t counter {0};
 };
 
-
 int main() {
-    cout.precision(17);
-    cout << scientific;
+    cout.precision(17); cout << scientific;
+    const auto p = 1. + complex<double>(0.,1.) * 2.;
+    const auto q = 2.;
+    cout << "Expected value = " << f_expected(p, q) << endl;
 
-    cx_double abserr;
-
-    using namespace quad1d;
-    auto p_sqr = 1. + cx_double(0.,1.) * 2.;
-    auto q = 2.;
-    ftor func(p_sqr, q);
-
-    Cag<decltype(func)> quad;
-    Cag_gsl<decltype(func)> quad_gsl;
-    
-    SPIT(f_expected(p_sqr, q));
-    auto res_gsl = quad_gsl.integrate(func, quad_gsl.neg_inf(), quad_gsl.pos_inf(), abserr);
-    SPIT(res_gsl);
-    SPIT(abserr);
-    SPIT(func.counter);
-
-    func.counter = 0;
-
-    auto res = quad.integrate(func, quad.neg_inf(), quad.pos_inf(), abserr);
-    SPIT(res);
-    SPIT(abserr);
-    SPIT(func.counter);
+    quad1d::Cag<Ftor> quad;
+    complex<double> abserr;
+    auto res = quad.integrate(Ftor(p, q), quad.neg_inf(), quad.pos_inf(), abserr);  //infinite interval
+    // For semi-infinite interval, use (lower, upper) = (quad.neg_inf(), b) or (a, quad.pos_inf()) 
+    cout << "Result = " << res << endl;
+    cout << "Estimated error = " << abserr << endl;
 }
